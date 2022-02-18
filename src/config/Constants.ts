ca334ab7b1b7e8612faad3f6adde354a76f2d332
Edmilson Torres <edmilson.torres@gmail.com>
2022-02-17 22:01:29 -0300

    feat: add dotenv (environment variables)

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,7 @@
+require('dotenv').config()
+
 export const config = {
 	API_URL: 'http://localhost:5000',
-	MONGO_CONNECTION:
-		'mongodb+srv://<user>:<password>@<database>.yptlq.mongodb.net/url-shortener-dio?retryWrites=true&w=majority',
+	MONGO_CONNECTION:	process.env.MONGO_CONNECT_URL,
+	// 'mongodb+srv://<user>:<password>@<database>.yptlq.mongodb.net/url-shortener-dio?retryWrites=true&w=majority',
 }
