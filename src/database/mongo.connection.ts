97a8f076df854c0b8929deede25cf1ee3dddfbd0
Edmilson Torres <edmilson.torres@gmail.com>
2022-02-18 14:35:01 -0300

    fix: file renamed

--- src/database/mongo.connection.ts
+++ src/database/mongo.connection.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 export class MongoConnection {
   public async connect (): Promise<void> {
     try {
-      await mongoose.connect(process.env.MONGO_CONNECT_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+      await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
       console.log('Database connected')
     } catch (err) {
       console.error(err.message)
