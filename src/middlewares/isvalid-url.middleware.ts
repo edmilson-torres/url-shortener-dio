9109f96eabe4f79b40bff849dc95fa996b22a099
Edmilson Torres <edmilson.torres@gmail.com>
2022-02-18 14:35:28 -0300

    fix: isValidUrl moved to utils

--- src/middlewares/isvalid-url.middleware.ts
+++ src/middlewares/isvalid-url.middleware.ts
@@ -1,15 +1,11 @@
 import { NextFunction, Request, Response } from 'express'
-
-function isValidURL (string: string) {
-  const res = string.match(/((?:([\w\d\.-]+)\:\/\/?){1}(?:(www)\.?){0,1}(((?:[\w\d-]+\.)*)([\w\d-]+\.[\w\d]+))){1}(?:\:(\d+)){0,1}((\/(?:(?:[^\/\s\?]+\/)*))(?:([^\?\/\s#]+?(?:.[^\?\s]+){0,1}){0,1}(?:\?([^\s#]+)){0,1})){0,1}(?:#([^#\s]+)){0,1}/gim)
-  return (res !== null)
-};
+import isValidURL from '../utils/is-valid-url.utils'
 
 const isValidURLMiddleware = (req: Request, resp: Response, next: NextFunction) => {
   const { originURL } = req.body
 
   if (!isValidURL(originURL)) {
-    resp.status(400).json({ error: 'Invalid URL' }).end()
+    resp.status(400).json({ error: 'Invalid base URL' })
   } else {
     next()
   }
---
+++ src/utils/is-valid-url.utils.ts
@@ -0,0 +1,6 @@
+function isValidURL (string: string) {
+  const res = string.match(/((?:([\w\d\.-]+)\:\/\/?){1}(?:(www)\.?){0,1}(((?:[\w\d-]+\.)*)([\w\d-]+\.[\w\d]+))){1}(?:\:(\d+)){0,1}((\/(?:(?:[^\/\s\?]+\/)*))(?:([^\?\/\s#]+?(?:.[^\?\s]+){0,1}){0,1}(?:\?([^\s#]+)){0,1})){0,1}(?:#([^#\s]+)){0,1}/gim)
+  return (res !== null)
+}
+
+export default isValidURL
